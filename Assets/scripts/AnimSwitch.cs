4d943d3c8935598e062cf2d04dff7e4f0a2781ba
Alex_s <adsalex77@gmail.com>
2024-09-26 15:57:17 +0300

    work in

---
+++ Assets/scripts/AnimSwitch.cs
@@ -0,0 +1,40 @@
+﻿using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AnimSwitch : MonoBehaviour,IPointerClickHandler {
+
+	Animator animator;
+	bool state;
+	
+	AudioSource audio;
+	void Start () {
+		animator = GetComponent<Animator>();
+        state = animator.GetBool("on");
+		audio = GetComponent<AudioSource>();	
+    }
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.M)) 
+		{
+            changeState();
+        }
+    }
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		changeState();
+
+    }
+
+    void changeState() 
+	{
+        state = !state;
+        animator.SetBool("on", state);
+        if (audio != null)
+            audio.Play();
+    }
+    }
