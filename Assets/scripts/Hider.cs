1c753d79d24d7d3e6f922f482430c5903bae535f
Alex_s <adsalex77@gmail.com>
2024-09-29 17:44:59 +0300

    glow_and_tips

---
+++ Assets/scripts/Hider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class Hider : MonoBehaviour {
+	// Use this for initialization
+	bool enabled= false;
+	Button btn;
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void fold() 
+	{ 
+		gameObject.SetActive(enabled) ;
+		enabled = !enabled;
+	}
+	
+
+}
---
+++ Assets/scripts/Tipper.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.EventSystems;
+
+public class Tipper : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler {
+	
+	static Text text;
+	static GameObject go;
+	static string defaultText = "Здесь отображаются подсказки к элементам установки";
+    [SerializeField]
+    string elementTip = "Это тест 1";
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        text.text = elementTip;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        text.text = defaultText;
+    }
+
+    // Use this for initialization
+
+
+    void Start () 
+	{
+        go = GameObject.Find("tipText");
+        text = go.GetComponent<Text>();
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+}
