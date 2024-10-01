d3046a8113ef2b9648b56a011423eaf84bbcc6c0
Alex_s <adsalex77@gmail.com>
2024-10-01 16:44:39 +0300

    menu wip

---
+++ Assets/scripts/PortalButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalButton : MonoBehaviour {
+	public Transform pose;
+	public float camDist;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/scripts/PortalMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class PortalMenu : MonoBehaviour {
+	[SerializeField]
+	Transform[] points;
+	[SerializeField]
+	GameObject button;
+	Button btn;
+    [SerializeField]
+    float[] camereDist;
+    [SerializeField]
+    string[] label;
+
+
+    // Use this for initialization
+    void Start () {
+		for(int i = 0;i<points.Length && i<camereDist.Length && i<label.Length;i++)
+		{
+			GameObject buffer = Instantiate(button, this.transform);
+			buffer.transform.Translate(0,-45*i,0);
+			PortalButton pb = buffer.GetComponent<PortalButton>();
+			pb.camDist= camereDist[i];
+            //pb.pose = points[i];
+			GameObject labelObj = buffer.transform.Find("Text").gameObject;
+			labelObj.GetComponent<Text>().text = label[i];
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void createBtn() 
+	{
+		Instantiate(button, this.transform);
+    }
+}
