d7cd0826744bacb814608f0ebf1c80bd2d144b70
Alex_s <adsalex77@gmail.com>
2024-10-13 14:52:16 +0300

    handles added

---
+++ Assets/scripts/BankHandleScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BankHandleScript : MonoBehaviour {
+
+	public int HandleValue { get; private set; }
+	void Start () {
+		HandleValue = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void HandleClick() 
+	{
+		transform.Rotate(Vector3.forward *36);
+		HandleValue++;
+		if (HandleValue >= 10) { HandleValue = 0; }
+	}
+}
