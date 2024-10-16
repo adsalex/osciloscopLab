02ee77ac069cdd223042603681390a835fdaaf60
Alex_s <adsalex77@gmail.com>
2024-10-16 11:53:05 +0300

    hysteresis clamped

--- Assets/scripts/StandScript.cs
+++ Assets/scripts/StandScript.cs
@@ -9,14 +9,24 @@ public class StandScript : MonoBehaviour {
 	LineRenderer line2;
 	const int posCount = 20;
     [SerializeField]
-    const float maxAmplitude = 0.15f;
+    const float maxAmplitude = 0.14f;
 	[SerializeField]
 	const float coilProportion =0.1f;
     float current1;
 	float voltage1;
 	float capacity;
-
+	
 	[SerializeField]
+	float ampModifyer = 2f;
+    [SerializeField]
+    float magnitudeModifyer = 1f;
+    [SerializeField]
+    float waveShift1 = 1f;
+    [SerializeField]
+    float waveShift2 = 1f;
+
+
+    [SerializeField]
 	BankHandleScript[] CapasitorHandles;
 	[SerializeField]
     BankHandleScript[] ResistorHandles;
@@ -24,18 +34,50 @@ public class StandScript : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		for (int i=0;i<posCount ;i++)
-		{
-			Vector3 buff = line1.GetPosition(i);
-			float f = Mathf.Sin(buff.x/maxAmplitude/2)*maxAmplitude;
-			buff.y = f;
-            //buff.y = -maxAmplitude + maxAmplitude / 10 * i;
-			line1.SetPosition(i, buff);
-		}
-	}
+		plotGraph(line1);
+        plotGraph(line2,waveShift2);
+    }
+	void plotGraphN(LineRenderer line,float additionalShift=0) 
+	{
+        for (int i = 0; i < posCount; i++)
+        {
+            Vector3 buff = line.GetPosition(i);
+            float f = Mathf.Sin(buff.x / (maxAmplitude) * ampModifyer + waveShift1+additionalShift) * maxAmplitude*magnitudeModifyer;
+            buff.y = f;
+            line.SetPosition(i, buff);
+        }
+    }
+
+    void plotGraph (LineRenderer line, float additionalShift = 0)
+    {
+        float maxVal = 0;  
+
+        for (int i = 0; i < posCount; i++)
+        {
+            Vector3 buff = line.GetPosition(i);
+            float x = buff.x / (maxAmplitude) * ampModifyer + waveShift1 + additionalShift;
+
+            x = Mathf.Clamp(x, -Mathf.PI / 2, Mathf.PI / 2);
+            float f = Mathf.Sin(x) * maxAmplitude * magnitudeModifyer;
+
+            
+            if (x >= 0 && x <= Mathf.PI / 2)
+            {
+                buff.y = f; 
+                maxVal = f; 
+            }
+            else
+            {
+               
+                buff.y = maxVal;             }
+
+            line.SetPosition(i, buff);
+        }
+    }
+
 }
