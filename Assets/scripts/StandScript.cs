1e51977cf9a813333edd810e510fef7122975c03
Alex_s <adsalex77@gmail.com>
2024-10-16 20:48:11 +0300

    loop working

--- Assets/scripts/StandScript.cs
+++ Assets/scripts/StandScript.cs
@@ -10,27 +10,37 @@ public class StandScript : MonoBehaviour {
 	const int posCount = 20;
     [SerializeField]
     const float maxAmplitude = 0.14f;
-	[SerializeField]
-	const float coilProportion =0.1f;
-    float current1;
-	float voltage1;
-	float capacity;
+	public float voltage1;
+	
+    public float capacity;
 	
 	[SerializeField]
 	float ampModifyer = 2f;
     [SerializeField]
     float magnitudeModifyer = 1f;
     [SerializeField]
-    float waveShift1 = 1f;
+    float allWavesShiftX = 1f;
+    
     [SerializeField]
-    float waveShift2 = 1f;
-
-
+    float secondWaveShiftX = 1f;
+    [SerializeField]
+    float secondWaveShiftY = 1f;
+    [SerializeField]
+    float maxVoltage = 220;
+    [SerializeField]
+    float resistance = 0;
+    [SerializeField]
+    float resistanceProportion = 1f;
+    float baseResistance = 1000f;
+    float voltagePercentage = 0.7f;
     [SerializeField]
 	BankHandleScript[] CapasitorHandles;
 	[SerializeField]
     BankHandleScript[] ResistorHandles;
 
+    float capacityProportion = 1f;
+    float baseCapacity = 1000f;
+    
 
     // Use this for initialization
     void Start () {
@@ -39,29 +49,23 @@ public class StandScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		plotGraph(line1);
-        plotGraph(line2,waveShift2);
+        resistanceProportion = resistance / baseResistance;
+        voltagePercentage = voltage1/maxVoltage;
+        capacityProportion = capacity / baseCapacity;
+		plotGraph(line1, -resistanceProportion * 0.5f, capacityProportion);
+        plotGraph(line2,resistanceProportion*0.5f,capacityProportion);
     }
-	void plotGraphN(LineRenderer line,float additionalShift=0) 
-	{
-        for (int i = 0; i < posCount; i++)
-        {
-            Vector3 buff = line.GetPosition(i);
-            float f = Mathf.Sin(buff.x / (maxAmplitude) * ampModifyer + waveShift1+additionalShift) * maxAmplitude*magnitudeModifyer;
-            buff.y = f;
-            line.SetPosition(i, buff);
-        }
-    }
+	
 
-    void plotGraph (LineRenderer line, float additionalShift = 0)
+    void plotGraph (LineRenderer line, float additionalShiftX = 0, float proportionMod = 1)
     {
         for (int i = 0; i < posCount; i++)
         {
             Vector3 buff = line.GetPosition(i);
-            float x = buff.x / (maxAmplitude) * ampModifyer + waveShift1 + additionalShift;
+            float x = (buff.x / maxAmplitude)*voltagePercentage * proportionMod * ampModifyer + allWavesShiftX*proportionMod*voltagePercentage + additionalShiftX;
 
             x = Mathf.Clamp(x, -Mathf.PI / 2, Mathf.PI / 2);
-            float f = Mathf.Sin(x) * maxAmplitude * magnitudeModifyer;
+            float f =( Mathf.Sin(x)) * voltagePercentage * maxAmplitude* proportionMod * magnitudeModifyer;
 
             buff.y = f;
             line.SetPosition(i, buff);
