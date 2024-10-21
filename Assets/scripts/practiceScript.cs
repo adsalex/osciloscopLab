9e571faea18070eb48b87d3e403bcfa2a84284e8
Alex_s <adsalex77@gmail.com>
2024-10-21 08:46:37 +0300

    wip

---
+++ Assets/scripts/practiceScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class practiceScript : MonoBehaviour {
+
+	public int actualStep =0;
+	int cuSt;
+	public int currentStep { get { return cuSt; } 
+		set { 
+			cuSt = value;
+            practiceText.text = Description[currentStep];
+		} 
+	}
+	public string[] Description;
+	[SerializeField]
+	Text practiceText;
+
+	void Start () {
+		currentStep = 0;
+	}
+
+	public void StepPlus() { if (currentStep < Description.Length-2 && currentStep<actualStep) currentStep++; }
+    public void StepMinus() { if (currentStep > 0) currentStep--;  }
+	public void addActualStep(int actual) 
+	{
+		actualStep += actual;
+		currentStep += 1;
+		
+	}
+}
---
+++ Assets/scripts/scenicEvent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scenicEvent : MonoBehaviour {
+	public int eventSteps=1;
+	
+	[SerializeField]
+	practiceScript ps;
+	int valForStep;
+	bool isUnused = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void eventTrue() { if (ps.actualStep == valForStep && isUnused) { ps.addActualStep(eventSteps); } }
+}
